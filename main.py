2d1258531d96095eff9049a8d07daef162ee0e04
Azamat <bakytbekovazamat45@gmail.com>
2022-05-17 21:35:58 +0600

    add new code

--- main.py
+++ main.py
@@ -76,5 +76,16 @@ print(""".
 
 
 
-print('один 1')
-print('два 2')
+print('My code 0')
+print('My code 1')
+print('это Адрей и он не курит :)')
+print("calc")
+print("hello world")
+
+print("Мой код")
+print("Мой код 2")
+
+print("Azamat  зачем ты запушил ENV???")
+
+
+print('Это Рустем)')
