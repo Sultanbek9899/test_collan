3067af7037e84b9d422ac3d67567ea5a421bceac
Aziz989898 <ishenkulov999@gmail.com>
2022-05-17 21:20:54 +0600

    jhkjk

--- main.py
+++ main.py
@@ -76,16 +76,5 @@ print(""".
 
 
 
-print('My code 0')
-print('My code 1')
-print('это Адрей и он не курит :)')
-print("calc")
-print("hello world")
-
-print("Мой код")
-print("Мой код 2")
-
-print("Azamat  зачем ты запушил ENV???")
-
-
-print('Это Рустем)')
+print('один 1')
+print('два 2')
