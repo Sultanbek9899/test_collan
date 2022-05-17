eba7f5138568f9ba101504275e40743c516b2444
Hasan <artykovh@gmail.com>
2022-05-17 21:49:16 +0600

    commit my branch

--- service.py
+++ service.py
@@ -1,3 +1,4 @@
+print("mlvkmlkvd vdnsdf,fE EGEJOITQNFE lmt ;oiWt .g' fQ;0;lw hfEFJQLfqnefmoa")
 print('Login from Askar')
 
 print("mlvkmlkvd vdnsdf,fE EGEJOITQNFE lmt ;oiWt .g' fQ;0;lw hfEFJQLfqnefmoa")
