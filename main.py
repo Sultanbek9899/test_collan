55b4ab8f0975e276a49a7c05ea575df986b5e33e
Aziz989898 <ishenkulov999@gmail.com>
2022-05-17 21:03:15 +0600

    git commit

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
- # [o, o, a]
 for i in range(1,200):
     print(i)
 print('My code 0')
 print('My code 1')
 print('это Адрей и он не курит :)')
-
-print('Мой код от Kwasli')
-print('хихиха')
 print("calc")
 print("hello world")
 
