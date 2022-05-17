8fe01193d07bc9d8a32d71cfc17343fa518e76cf
312askar <askar.shekenov@yandex.ru>
2022-05-17 21:43:01 +0600

    add login in service

--- service.py
+++ service.py
@@ -1,4 +1,9 @@
+print('Login from Askar')
+
+print("My name is Sadbringer")
 print("mlvkmlkvd vdnsdf,fE EGEJOITQNFE lmt ;oiWt .g' fQ;0;lw hfEFJQLfqnefmoa")
+
+
 print('Login from Askar')
 
 print("mlvkmlkvd vdnsdf,fE EGEJOITQNFE lmt ;oiWt .g' fQ;0;lw hfEFJQLfqnefmoa")
