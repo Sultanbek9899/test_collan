515a0aa84655f6d999169b0835c69f7f927b6b46
altai <kozhobekovaltaiazamat@gmail.com>
2022-05-17 21:36:08 +0600

    ads

--- main.py
+++ main.py
@@ -78,3 +78,17 @@ print(""".
 
 print('один 1')
 print('два 2')
+print('My code 0')
+print('My code 1')
+print('это Адрей и он не курит :)')
+print("calc")
+print("hello world")
+print("Мой код")
+print("Мой код 2")
+
+print("Azamat  зачем ты запушил ENV???")
+
+
+print('Это Рустем)')
+
+print('купите мне шаурму')
