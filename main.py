bfda6a868576011e3107588941311421fbc2ce8d
Dauren <daurenjenbaev@gmail.com>
2022-05-17 21:10:04 +0600

    update commit

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ for i in range(1,200):
 print('My code 0')
 print('My code 1')
 print('это Адрей и он не курит :)')
-
 print('Мой код от Kwasli')
 print('хихиха')
 print("calc")
 print("hello world")
-
 print("Мой код")
 print("Мой код 2")
 
